535eb5b1f78ca539a8f89968793163f32f6243f2
SANDHYA  SHEVATRE <141001905+sandhyashevatre@users.noreply.github.com>
2023-08-21 00:44:22 +0530

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,14 @@
+package Tennis_game1;
+
+public class Point {
+    private Player winner;
+
+    public Point(Player winner) {
+        this.winner = winner;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+}
+
